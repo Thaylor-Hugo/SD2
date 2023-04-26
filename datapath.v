f9033f85cb018909fff5617251ec76115577dd51
Thaylor-Hugo <thaylorhugo@gmail.com>
2023-04-26 09:27:38 -0300

    ✨ Adicionar operacoes de Add e Sub

--- datapath.v
+++ datapath.v
@@ -12,28 +12,38 @@ module datapath #(
     input enable,
     input [4:0] ra, 
     input [4:0] rb,
-    input [BITS:0] dataIn,
-    input load_store, //1 se for load, 0 se for store
+    input signed [BITS:0] dataIn,
+    input load_store,       //1 se for load ou tipo R, 0 se for store
+    input op_ula,           //1 se for soma, 0 se for sub
+    input operation_type,   //1 se for tipo R, 0 se for memory type
+    input ula_entry,        //1 se for Reg, 0 se for imm
     input [4:0] rw,
 
-    output [BITS:0] dataOutA, 
-    output [BITS:0] dataOutB 
+    output signed [BITS:0] dataOutA, 
+    output signed [BITS:0] dataOutB 
 );
 
-wire [BITS:0] valor_regA;
-wire [BITS:0] valor_regB;
+wire signed [BITS:0] valor_regA;
+wire signed [BITS:0] valor_regB;
 wire v; // overflow da soma
 wire [BITS:0] mem_read;
-wire [BITS:0] endereco;
+wire [BITS:0] ula_out;
 wire load_en;
 wire store_en;
+wire signed [BITS:0] din_reg;
+wire signed [BITS:0] din_ula;
 
 assign load_en = (enable)? load_store: 0;
 assign store_en = (enable)? ~load_store: 0;
 
-somador somador(dataIn, dataOutB, 1'b1, v, endereco);
-banco_reg banco_reg (clk, load_en, rw, ra, rb, mem_read, valor_regA, valor_regB);
-ram ram (clk, store_en, endereco, valor_regA, mem_read);
+// Decide entrada do banco de registradores
+mux_two_to_one mux_banco_reg(operation_type, ula_out, mem_read, din_reg);
+// Decide entrada da ULA
+mux_two_to_one mux_ula(ula_entry, valor_regA, dataIn, din_ula);
+
+somador somador(dataOutB, din_ula, op_ula, v, ula_out);
+banco_reg banco_reg (clk, load_en, rw, ra, rb, din_reg, valor_regA, valor_regB);
+ram ram (clk, store_en, ula_out, valor_regA, mem_read);
 
 assign dataOutA = valor_regA;
 assign dataOutB = valor_regB;
--- test_benchs/tb_datapath.v
+++ test_benchs/tb_datapath.v
@@ -18,8 +18,14 @@ module tb_datapath;
     reg enable = 0;
     reg [4:0] a, b, w;
     reg load_store;
-    reg [63:0] din;
-    wire [63:0] douta, doutb;
+    reg signed [63:0] din;
+
+    // adicionados para adicicao de operacoes de add e sub
+    reg op_ula;
+    reg operation_type;
+    reg ula_entry;
+
+    wire signed [63:0] douta, doutb;
 
     initial begin
         $dumpfile("tb_datapath.vcd");
@@ -28,12 +34,15 @@ module tb_datapath;
 
     always #1 clk = !clk;
 
-    datapath datapath (clk, enable, a, b, din, load_store, w, douta, doutb);
+    datapath datapath (clk, enable, a, b, din, load_store, op_ula, operation_type, ula_entry, w, douta, doutb);
 
     initial begin
 
         //test bench do load e do store:
         enable = 1;
+        op_ula = 1;
+        operation_type = 0;
+        ula_entry = 0;
 
         #2; // store value of reg[2] in mem[0+reg[0]] -> mem[0] = 2;
         load_store=0;
@@ -69,19 +78,77 @@ module tb_datapath;
         din= 64'd8;
         w=  5'd31;
         b=  5'd0;
+
+        // Testbench para operacoes de add e sub com imm    (regW = regB +- imm)
+        #2;
+        load_store = 1;
+        operation_type = 1;
+        ula_entry = 0;
+        op_ula = 1;
+        
+        din = -64'd85;
+        b = 5'd3;
+        w = 5'd15;
+
+        #2;
+        op_ula = 0;
+        din = -64'd42;
+        b = 5'd0;
+        w = 5'd16;
+
+        // Testbench para operacoes de add e sub    (regW = regB +- regA)
+        #2;
+        ula_entry = 1;
+        op_ula = 1;
+        b = 5'd15;
+        a = 5'd16;
+        w = 5'd17;
+
+        #2;
+        op_ula = 0;
+        b = 5'd0;
+        a = 5'd3;
+        w = 5'd18;
+
+        #2;
+        op_ula = 1;
+        b = 5'd17;
+        a = 5'd18;
+        w = 5'd19;
+
+        #2;
+        op_ula = 0;
+        b = 5'd16;
+        a = 5'd27;
+        w = 5'd20;
+
+        // Checa se as operacoes funcionaram pelos valores esperados nos registradores
+        // Operacoes de load e store 
         
         #2;
         enable = 0;
-
-        // Checa se as operacoes funcionaram pelos valores esperados nos registradores
-
-        #2;
         a=5'd2; // reg[2] = 16
         b=5'd3; // reg[3] = 31
 
         #2;
         a=5'd30; // reg[30] = 2
         b=5'd31; // reg[31] = 4
+
+        // operacoes de add e sub com imm
+
+        #2;
+        a=5'd15; // reg[15] = -54
+        b=5'd16; // reg[16] = 42
+
+        // Operacoes de add e sub
+
+        #2;
+        a=5'd17; // reg[17] = -12
+        b=5'd18; // reg[18] = -31
+        
+        #2;
+        a=5'd19; // reg[19] = -43
+        b=5'd20; // reg[20] = 15
     
         #2 $finish;
 
