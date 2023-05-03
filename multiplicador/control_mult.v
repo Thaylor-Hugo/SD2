148edd9059251c8967beb60e3c2eb24d7a4ad07a
Thaylor-Hugo <thaylorhugo@gmail.com>
2023-05-02 22:31:18 -0300

    🎨 Aperfeiçoamento do control_mult

--- multiplicador/control_mult.v
+++ multiplicador/control_mult.v
@@ -1,7 +1,7 @@
 module control_mult (
     input clk,
     input start,
-    output done,
+    output reg done,
     output reg op_somador,
     output reg [1:0] op_mux1,
     output reg [1:0] op_mux2,
@@ -10,49 +10,64 @@ module control_mult (
     output reg [2:0] op_mux5
 );
 
-always @(posedge start) begin
-    // Carrega valor de A
-    // Carrega valor de D
-    op_somador = 1;
-    op_mux1 = 2'b00;
-    op_mux2 = 2'b00;
-    op_mux3 = 3'b000;
-    op_mux4 = 3'b000;
-    op_mux5 = 3'b001;
+reg [2:0] state;
 
-    @(posedge clk);
-    // Carrega valor de B
-    // Carrega valor de E
-    op_mux1 = 2'b01;
-    op_mux2 = 2'b01;
-    op_mux3 = 3'b001;
-    op_mux4 = 3'b001;
-    op_mux5 = 3'b010;
-    
-    @(posedge clk);
-    // Multiplica D e E
-    // Soma A e B
-    op_mux1 = 2'b10;
-    op_mux2 = 2'b10;
-    op_mux3 = 3'b010;
-    op_mux4 = 3'b010;
-    op_mux5 = 3'b000;
-
-    @(posedge clk);
-    // Subtrai de e a_b
-    op_somador = 0;
-    op_mux3 = 3'b011;
-    op_mux4 = 3'b011;
-    op_mux5 = 3'b011;
-
-    @(posedge clk);
-    // Acha resultado da multitiplicacao
-    op_somador = 1;
-    op_mux3 = 3'b100;
-    op_mux4 = 3'b100;
-    op_mux5 = 3'b100;
+always @(posedge clk) begin
+    case (state)
+        3'b000: begin
+            // Carrega valor de A
+            // Carrega valor de D
+            op_somador = 1;
+            op_mux1 = 2'b00;
+            op_mux2 = 2'b00;
+            op_mux3 = 3'b000;
+            op_mux4 = 3'b000;
+            op_mux5 = 3'b001;
+            state = 3'b001;
+        end
+        3'b001: begin
+            // Carrega valor de B
+            // Carrega valor de E
+            op_mux1 = 2'b01;
+            op_mux2 = 2'b01;
+            op_mux3 = 3'b001;
+            op_mux4 = 3'b001;
+            op_mux5 = 3'b010;
+            state = 3'b010;
+        end
+        3'b010: begin
+            // Multiplica D e E
+            // Soma A e B
+            op_mux1 = 2'b10;
+            op_mux2 = 2'b10;
+            op_mux3 = 3'b010;
+            op_mux4 = 3'b010;
+            op_mux5 = 3'b000;
+            state = 3'b011;
+        end
+        3'b011: begin
+            // Subtrai de e a_b
+            op_somador = 0;
+            op_mux3 = 3'b011;
+            op_mux4 = 3'b011;
+            op_mux5 = 3'b011;
+            state = 3'b100;
+        end
+        3'b100: begin
+            // Acha resultado da multitiplicacao
+            op_somador = 1;
+            op_mux3 = 3'b100;
+            op_mux4 = 3'b100;
+            op_mux5 = 3'b100;
+            state = 3'b101;
+            done = 1'b1;
+        end
+    endcase
 end
 
-assign done = (op_mux5 == 3'b100)? 1 : 0;
+always @(posedge start) begin
+    state = 3'b000;
+    done = 0;
+end
 
 endmodule
