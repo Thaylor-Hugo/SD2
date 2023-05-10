66dcc53fd01258b2194641c61c55aefb8b5feb7e
Thaylor-Hugo <thaylorhugo@gmail.com>
2023-05-09 22:11:45 -0300

    🐛 Corrige index do vetor

--- memoria_instrucao.v
+++ memoria_instrucao.v
@@ -11,12 +11,12 @@ module memoria_instrucao (
         rom[0] = 32'b0; // Do nothing
         rom[1] = 32'b00000000011100000010000010000011; // load mem[7+reg[0]] into reg[1] -> reg[1] = 7 
         rom[2] = 32'b00000001010100111000111110110011; // add 31$ 7$ 21$    ->  reg[31] = 28
-        rom[3] = 32'b01000001111110001000111100110011; // sub 30$ 17$ 31$   ->  reg[30] = -14
+        rom[3] = 32'b01000001111110001000111100110011; // sub 30$ 17$ 31$   ->  reg[30] = -11
         rom[4] = 32'b00000101000100101000111010010011; // addi 29$ 5$ #81   ->  reg[29] = 86
         rom[5] = 32'b00000010101000011000111000011111; // subi 28$ 3$ #42   ->  reg[28] = -39        // nao ha subi on risc v, entao usando combinacao de opcode que nao e usada por outra instrucao
-        rom[5] = 32'b00000011110111100010010000100011; // store reg[29] in mem[40+reg[28]] -> mem[1] = 86
+        rom[6] = 32'b00000011110111100010010000100011; // store reg[29] in mem[40+reg[28]] -> mem[1] = 86
 
-        for (i=6; i<64; i=i+1)
+        for (i=7; i<64; i=i+1)
             rom[i] = 32'b0;
     end  
     
